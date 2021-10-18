876f35e5e7074e56086b103a6267ff91c82cd4f7
Tai Fa Tang <david_445566@hotmail.com>
2021-10-18 11:08:15 +0100

    Update Readme.cs

--- Tests/RestaurantSearchService.AcceptanceTests/Readme.cs
+++ Tests/RestaurantSearchService.AcceptanceTests/Readme.cs
@@ -5,7 +5,7 @@
         /*
          * I was suppose to do this too but ran out of time
          * In this file I would test E2E where the service will actually make a call to Just Eat
-         * It's likely to be a test outCode Just Eat Api expects.
+         * It's likely to be a test outCode Just Eat Api expects. e.g. ec4m
          */
     }
 }
